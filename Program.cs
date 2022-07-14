eddd1b390d7ebd7c4bb246e661970ad1c2870770
kamil7766 <kamil.lopata.767@gmail.com>
2022-07-14 17:39:44 +0200

    Create Program.cs
    Początek pracy

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintRainMessage
+{
+    /// <summary>
+    /// Prints a rain message
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Prints a rain message
+        /// </summary>
+        /// <param name="args">command-line args</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world");
+            Console.WriteLine("Chinese Democracy is done and it's November");
+            Console.WriteLine("Is it raining?");
+            Console.WriteLine();
+           
+        }
+    }
+}
